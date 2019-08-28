550a313c065b164122f3a878b566cd2e3b184257
bucr <whut_bcr@163.com>
2019-08-28 10:15:43 +0800

    update to builder

--- src/main/java/com/bucr/util/KingbaseConnector.java
+++ src/main/java/com/bucr/util/KingbaseConnector.java
@@ -2,40 +2,115 @@ package com.bucr.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
+import java.sql.Statement;
 
 public class KingbaseConnector {
-  private static final String DB_PREFIX = "jdbc:kingbase://";
-  private Connection conn = null;
-  private String url;
-  private String user;
-  private String password;
-  public KingbaseConnector(String host, String port, String database, String user, String password){
-    url = DB_PREFIX + host + ":" + port + "/" + database;
-    this.user = user;
-    this.password = password;
-  }
 
-  public Connection getConn() {
+  private static final String DB_PREFIX = "jdbc:kingbase://";
+
+  static {
     try {
       Class.forName("com.kingbase.Driver");
-      conn = DriverManager.getConnection(url, user, password);
-      if(conn != null) {
-        return conn;
-      } else {
-        System.out.println(this.getClass().getName() + ": Get connection FAILED! Connection is NULL!");
-      }
     } catch (ClassNotFoundException e) {
-      System.out.println(this.getClass().getName() + ": Cannot found \"com.kingbase.Driver\". Exception need to handle.");
-      e.printStackTrace();
-    } catch (SQLException e) {
-      System.out.println(this.getClass().getName() + ": Get connection FAILED. Exception need to handle.");
       e.printStackTrace();
     }
   }
 
-  public List<String> getTablesName(){
+  private String url;
+  private String user;
+  private String password;
 
+
+  private KingbaseConnector(Builder builder) {
+    url = builder.url;
+    user = builder.user;
+    password = builder.password;
+  }
+
+  public ResultSet getTables(String catalog, String schemaPattern, String tableNamePattern,
+      String[] types) throws SQLException {
+    return getConnection().getMetaData().getTables(catalog, schemaPattern, tableNamePattern, types);
+  }
+
+  public Connection getConnection() {
+    Connection conn = null;
+    try {
+      conn = DriverManager.getConnection(url, user, password);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return conn;
+  }
+
+  public void closeConnectionAndStatement(Connection connection, Statement statement) {
+    closeConnection(connection);
+    closeStatement(statement);
+  }
+
+  public void closeConnection(Connection connection) {
+    try {
+      if (connection != null) {
+        connection.close();
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void closeStatement(Statement statement) {
+    try {
+      if (statement != null) {
+        statement.close();
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static class Builder {
+
+    private String url = "localhost";
+    private String hostname = "localhost";
+    private String port = "54321";
+    private String database = "";
+    private String user = "SYSTEM";
+    private String password = "krms";
+
+    public Builder hostname(String hostname) {
+      this.hostname = hostname;
+      return this;
+    }
+
+    public Builder port(String port) {
+      this.port = port;
+      return this;
+    }
+
+    public Builder databse(String database) {
+      this.database = database;
+      return this;
+    }
+
+    public Builder user(String user) {
+      this.user = user;
+      return this;
+    }
+
+    public Builder password(String password) {
+      this.password = password;
+      return this;
+    }
+
+    private String url() {
+      return DB_PREFIX + hostname + ":" + port + "/" + database;
+    }
+
+    public KingbaseConnector build() {
+      url = url();
+      return new KingbaseConnector(this);
+    }
   }
 }
+
--- src/main/java/com/bucr/util/MysqlConnector.java
+++ src/main/java/com/bucr/util/MysqlConnector.java
@@ -1,19 +1,131 @@
 package com.bucr.util;
 
+
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MysqlConnector {
+
   private static final String DB_PREFIX = "jdbc:mysql://";
-  private String user = "root";
-  private String password = "123456";
+
+  static {
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
   private String url;
-  private Connection conn = null;
+  private String user;
+  private String password;
 
-  public MysqlConnector(String hostname, String port, String database, String user, String password){
-    url = DB_PREFIX
-        + (hostname == null ? "localhost" : hostname) + ":"
-        + (port == null ? "3306" : port) + "/"
-        + (database == null ? "" : database);
+  private MysqlConnector(Builder builder){
+    url = builder.url;
+    user = builder.user;
+    password = builder.password;
+  }
 
+  public void createDatabase(String database) {
+    String sql = "CREATE DATABASE ?";
+    Connection conn = getConnection();
+    PreparedStatement ps = null;
+    try {
+      ps = conn.prepareStatement(sql);
+      ps.setString(1, database);
+      ps.executeUpdate();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } finally {
+      closeConnectionAndStatement(conn, ps);
+    }
+  }
+
+  public ResultSet getTables(String catalog, String schemaPattern, String tableNamePattern,
+      String[] types) throws SQLException {
+    return getConnection().getMetaData().getTables(catalog, schemaPattern, tableNamePattern, types);
+  }
+
+  public Connection getConnection() {
+    Connection conn = null;
+    try {
+      conn = DriverManager.getConnection(url, user, password);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return conn;
+  }
+
+  public void closeConnectionAndStatement(Connection connection, Statement statement) {
+    closeConnection(connection);
+    closeStatement(statement);
+  }
+
+  public void closeConnection(Connection connection) {
+    try {
+      if (connection != null) {
+        connection.close();
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void closeStatement(Statement statement) {
+    try {
+      if (statement != null) {
+        statement.close();
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static class Builder {
+
+    private String url = "localhost";
+    private String hostname = "localhost";
+    private String port = "3306";
+    private String database = "";
+    private String user = "root";
+    private String password = "krms";
+
+    public Builder hostname(String hostname) {
+      this.hostname = hostname;
+      return this;
+    }
+
+    public Builder port(String port) {
+      this.port = port;
+      return this;
+    }
+
+    public Builder databse(String database) {
+      this.database = database;
+      return this;
+    }
+
+    public Builder user(String user) {
+      this.user = user;
+      return this;
+    }
+
+    public Builder password(String password) {
+      this.password = password;
+      return this;
+    }
+
+    private String url() {
+      return DB_PREFIX + hostname + ":" + port + "/" + database;
+    }
+
+    public MysqlConnector build() {
+      url = url();
+      return new MysqlConnector(this);
+    }
   }
 }
