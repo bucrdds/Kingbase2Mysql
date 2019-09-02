407eb12faee33636e364bfacda9e8b5e82a13fdd
bucr <whut_bcr@163.com>
2019-09-02 16:16:39 +0800

    cannot read foreign keys

---
+++ src/test/java/com/bucr/util/KingbaseConnectorTest.java
@@ -0,0 +1,24 @@
+package com.bucr.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class KingbaseConnectorTest {
+
+  @Test
+  void getTables() {
+  }
+
+  @Test
+  void getColumns() {
+  }
+
+  @Test
+  void getPrimaryKeys() {
+  }
+
+  @Test
+  void getForeignKeys() {
+  }
+}
